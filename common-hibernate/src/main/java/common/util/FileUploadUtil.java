3077840d4a86c25f21df9d20b13921d766ff132f
HelloKitty1991 <peng19956@sina.com>
2016-04-10 13:44:14 +0800

    upload

--- common-hibernate/src/main/java/common/util/FileUploadUtil.java
+++ common-hibernate/src/main/java/common/util/FileUploadUtil.java
@@ -1,13 +1,12 @@
 package common.util;
 
-import java.io.File;
 import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartRequest;
+
+import com.aliyun.oss.OSSClient;
 
 import common.bo.ResultFlag;
 
@@ -28,6 +27,23 @@ public class FileUploadUtil {
 	 */
 	private String endpoint;
 	
+	/**
+	 * 阿里云oss存储包名称
+	 */
+	private String bucketName;
+	/**
+	 * 阿里云oss资源读取地址
+	 */
+	private String readBaseUrl;
+	
+	public void setReadBaseUrl(String readBaseUrl) {
+		this.readBaseUrl = readBaseUrl;
+	}
+
+	public void setBucketName(String bucketName) {
+		this.bucketName = bucketName;
+	}
+
 	public void setEndpoint(String endpoint) {
 		this.endpoint = endpoint;
 	}
@@ -41,20 +57,16 @@ public class FileUploadUtil {
 	}
 	
 	
-	public ResultFlag upload(MultipartRequest request, MultipartFile files,String filePath) throws Exception {
+	public ResultFlag upload(MultipartRequest request, MultipartFile files,String type) throws Exception {
 		String originalFileName=files.getOriginalFilename();
 		String extName=originalFileName.substring(originalFileName.lastIndexOf(".")+1);
 		String newFileName=UUID.randomUUID().toString()+"."+extName;
-		HttpServletRequest req=(HttpServletRequest)request;
-		String realPath=req.getSession().getServletContext().getRealPath(filePath);
-		File file=new File(realPath+File.separator+newFileName);
-		if(!file.exists()){
-			file.getParentFile().mkdirs();
-		}
-		files.transferTo(file);
+		OSSClient client = new OSSClient(endpoint,accessKeyId,accessKeySecret);
+		String  key = type+"/"+newFileName;
+		client.putObject(bucketName, key, files.getInputStream());
 		ResultFlag flag=new ResultFlag();
 		flag.setFlag(true);
-		flag.setData("{'url':'/"+filePath+"/"+newFileName+"','fileType':'"+extName+"','state':'SUCCESS','original':'"+originalFileName+"'}");
+		flag.setData("{'url':'"+readBaseUrl+"/"+key+"','fileType':'"+extName+"','state':'SUCCESS','original':'"+originalFileName+"'}");
 		return flag;
 	}
 }
